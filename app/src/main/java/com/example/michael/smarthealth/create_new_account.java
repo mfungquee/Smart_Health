1d1efbf4d775c224650082343bf3f2deb5930612
mfungquee <mikerampage117@gmail.com>
2017-04-10 10:07:36 -0400

    Initial commit

---
+++ app/src/main/java/com/example/michael/smarthealth/create_new_account.java
@@ -0,0 +1,7 @@
+package com.example.michael.smarthealth;
+
+/**
+ * Created by Michael on 2/14/2017.
+ */
+public class create_new_account {
+}
---
+++ app/src/main/java/com/example/michael/smarthealth/main_screen.java
@@ -0,0 +1,7 @@
+package com.example.michael.smarthealth;
+
+/**
+ * Created by Michael on 4/2/2017.
+ */
+public class main_screen {
+}
