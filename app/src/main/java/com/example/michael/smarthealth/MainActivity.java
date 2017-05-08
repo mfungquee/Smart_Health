6d2eeb1c6cf6f4e5a3405c1e46f3e00fb75a3b38
mfungquee <mikerampage117@gmail.com>
2017-05-07 21:14:24 -0400

    Buttons move from page to page Some New Java files Some changes in xml designs Added all the layout files into the Manifest Gradle version updated

--- app/src/main/java/com/example/michael/smarthealth/MainActivity.java
+++ app/src/main/java/com/example/michael/smarthealth/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.michael.smarthealth;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    //xml ids of buttons
+    public Button buttonCreateNewAccount;
+    public Button buttonLogin;
+
+    //functions for UI interactions
+    public void functionsUI(){
+
+        buttonCreateNewAccount = (Button)findViewById(R.id.buttonCreateNewAccount); //reference to button from xml
+
+        buttonCreateNewAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent1 = new Intent(v.getContext(), create_new_account.class); //intent is the link between pages
+
+                startActivity(intent1); //when button is pressed, move from activity1 to activity2
+
+                //Intent intent = create_new_account.makeIntent(MainActivity.this);
+                //startActivity(intent);
+            }
+        }); //end line of buttonCreateNewAccount
+
+        buttonLogin = (Button)findViewById(R.id.buttonLogin); // reference to button from xml
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent2 = new Intent(v.getContext(), login.class); // intent = link between main and login page
+
+                startActivity(intent2); //move from main to login
+            }
+        }); //end line of buttonLogin
+    }//end functionsUI
+
+    //creates the initial screen
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        functionsUI(); //UI functionality
     }
+
 }
